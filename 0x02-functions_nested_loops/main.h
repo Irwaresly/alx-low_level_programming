b584b61f01e01f882af2099eca0715817fb3ab78
Irwaresly <silviairware372@gmail.com>
2023-06-21 03:01:11 -0700

    prints putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-
-int int_putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +10,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
